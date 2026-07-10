9876a0ee020ac774a9a41cf57cc497a2b7d87b33
Boris_Perez <bo.perezb@duocuc.cl>
2026-07-10 15:33:13 -0400

    Configura HotelPro para Docker y despliegue en Render

---
+++ hotelpro-gateway/src/main/java/duoc/hotelpro_gateway/HotelproGatewayApplication.java
@@ -0,0 +1,13 @@
+package duoc.hotelpro_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelproGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelproGatewayApplication.class, args);
+	}
+
+}
---
+++ hotelpro-gateway/src/test/java/duoc/hotelpro_gateway/HotelproGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.hotelpro_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelproGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
